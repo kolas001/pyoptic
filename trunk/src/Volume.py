ea7f352c49cc1367bde984660a022a90e1ed8210
stewart.boogert <stewart.boogert@bc6326ce-6316-11de-b259-67f869b4f23a>
2009-08-17 13:50:41 +0000

    Moved most functionality to Elements

--- trunk/src/Volume.py
+++ trunk/src/Volume.py
@@ -4,21 +4,3 @@ from Base      import *
 from Material  import *
 from Placement import *
 
-class Volume:
-    """ Orientable volume, base class to describe extent, material and orientation of a volume containing """ 
-
-    # shape of element
-    rect = 0
-    circ = 1 
-            
-    def __init__(self,shape,dimension,placement,material) :
-        self.shape     = shape
-        self.dimension = dimension
-        self.placement = placement
-        self.material  = material
-        print str(self)
-
-    def  __str__(self) :
-        return "Volume(shape="+str(self.shape)+",dimension="+str(self.dimension)+",placement="+str(self.placement)+",material="+str(self.material)+")"
-    
-    
