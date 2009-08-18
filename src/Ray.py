cf08e6ed33a357dfae679cabbd57363de797f116
stewart.boogert <stewart.boogert@bc6326ce-6316-11de-b259-67f869b4f23a>
2009-08-18 17:08:59 +0000

    Added spherical intersection, Snell's law and reflection law

--- src/Ray.py
+++ src/Ray.py
@@ -1,15 +1,22 @@
 import pylab as pl
 
 class Ray :
-    """ Class describing a light ray from an initial point to a final point """
+    ''' Class describing a light ray from an initial point to a final point '''
 
     def __init__(self, point, dir) :
+        '''Construct a ray from a start point and direction dir'''
         print "Ray:__init__>"
         
         # initial points of the ray
-        self.p0 = point 
-        self.d  = dir
+        self.p0 = pl.array(point)
+        self.d  = pl.array(dir)
+        self.d  = self.d/pl.norm(self.d)
 
-        # values after the ray has been propagated
-        self.p1 = pl.array([])
-        self.pha = 0 
+        # values after the ray has been propagated (empty to start with)
+        self.p1 = None
+        self.pha = 0        
+
+
+    def propagate(self, lam) :
+        return lam*self.d + self.p0
+
